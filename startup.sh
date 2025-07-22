a8f1ed19dc188964e6166355adf4f1eb9f180f3e
jorgealves96 <nrgtik1337@hotmail.com>
2025-07-22 17:24:14 +0100

    fsdbfwe

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This command applies any pending migrations
-dotnet ef database update
+# dotnet ef database update  <-- COMMENT THIS LINE OUT
 
 # This command starts your actual application
 dotnet LearningAppNetCoreApi.dll
