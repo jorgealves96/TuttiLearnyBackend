16922c04d3962b62d83da63040e86bf57b67e934
jorgealves96 <nrgtik1337@hotmail.com>
2025-07-31 14:06:14 +0100

    - Changed path subscription constant values

--- Constants/SubscriptionConstants.cs
+++ Constants/SubscriptionConstants.cs
@@ -2,10 +2,10 @@
 {
     public static class SubscriptionConstants
     {
-        public const int FreePathGenerationLimit = 5;
-        public const int FreePathExtensionLimit = 5;
+        public const int FreePathGenerationLimit = 3;
+        public const int FreePathExtensionLimit = 1;
 
-        public const int ProPathGenerationLimit = 25;
-        public const int ProPathExtensionLimit = 25;
+        public const int ProPathGenerationLimit = 15;
+        public const int ProPathExtensionLimit = 15;
     }
 }
