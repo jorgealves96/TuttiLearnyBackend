9964b5c7ef1a73137b04c83a8f9727b5971e218d
jorgealves96 <nrgtik1337@hotmail.com>
2025-08-09 18:38:49 +0100

    Change usehttpsredirection to not be used in production, so that cloud logs don't complain about "Failed to determine the https port for redirect."

--- Program.cs
+++ Program.cs
@@ -130,7 +130,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 app.UseAuthentication();
 app.UseAuthorization();
 
